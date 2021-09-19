77a56a0fc924d4793be9ac92fe6d8f6279af6138
Vadim Gulakov <Haker.201@yandex.ru>
2021-09-19 17:39:49 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ direction = os.getcwd()
 
 
 files = os.listdir(direction)
-
 extensions = [file.split('.')[1] for file in files if os.path.isfile(file)]
+
 while True:
-    for extension in extensions[1:]:
+    for extension in extensions:
         if not os.path.exists(extension):
             os.mkdir(extension)
     for file in files:
-        if file == 'main.py' or file == '.idea':
+        if file == 'main.py' or len(file.split('.')[0]) == 0:
             continue
         os.replace(file, f"{file.split('.')[1]}/{file}")
     break
+
